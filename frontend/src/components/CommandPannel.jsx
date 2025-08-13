13d7dfe4a12c663ea6c9c52633c2126191fb2e16
Enzo Silveira <163486167+enzosas@users.noreply.github.com>
2025-08-13 16:56:21 -0300

    inicio painelcomando

--- frontend/src/components/CommandPannel.jsx
+++ frontend/src/components/CommandPannel.jsx
@@ -5,10 +5,21 @@ function CommandPannel(){
         <div>
             <div className="frame-checkbox-painel">
                 <input type="checkbox"></input>
-                <label >Painel de Comando</label><br></br>
+                <label >Painel de Comando</label>
             </div>
-            <div className="frame-resto">
-                
+            <div className="frame-branco">
+                <h1>Painel de Comando</h1>
+                <hr></hr>
+                <select name="cars" id="cars">
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                </select>
+                <label class="checkbox-item disabled">
+                    <input type="checkbox" disabled></input>
+                    Dupla Velocidade de Elevação com inversor de frequência
+                </label>
             </div>
         </div>
     )
