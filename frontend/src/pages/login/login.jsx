3db0b7ec980f364b1f9fb77e6dfab3ebe2d5899f
Enzo Silveira <163486167+enzosas@users.noreply.github.com>
2025-10-29 12:10:02 -0300

    ta indo

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -7,27 +7,56 @@ function Login() {
     
     const [senha, setSenha] = useState('');
 
-    const handleInputChange = (evento) => {
-        setNome(evento.target.value);
+    const handleUsuarioChange = (evento) => {
+        setUsuario(evento.target.value);
+    };
+
+    const handleSenhaChange = (evento) => {
+        setSenha(evento.target.value);
     };
 
 
     return (
-        <div className="login__frame-branco">
-            <h1>Login</h1>
-            
-            <div className="login__frame-branco__labelCampo">
-            <label>
-                Nome
-            </label>
-                <input 
-                    type="text" 
-                    value={usuario} 
-                    onChange={handleInputChange} 
+        <div className="margem">
+            <div className="header">
+                <img
+                    src="tcsind.png"
+                    alt="TCS Indústria Metalúrgica"
+                    className="header-logo"
+                    style={{ maxHeight: '3rem' }}
                 />
+                <h1>Gerador de Orçamento</h1>
             </div>
-            
+            <div className="login__frame-branco">
+                
+                <h1>Login</h1>
+                <div className="login__frame-branco__labelCampo ">
+                    <label>
+                        Usuário
+                    </label>
+                    <input 
+                        type="text" 
+                        value={usuario} 
+                        onChange={handleUsuarioChange} 
+                    />
+                </div>
+                <div className="login__frame-branco__labelCampo ">
+                    <label>
+                        Senha
+                    </label>
+                    <input 
+                        type="text" 
+                        value={senha} 
+                        onChange={handleSenhaChange} 
+                    />
+                </div>
+                <button type="button" className="botao">
+                    Entrar
+                </button>
+                
+            </div>
         </div>
+        
     )
 }
 
