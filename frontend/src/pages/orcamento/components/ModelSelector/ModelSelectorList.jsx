d961a3e6611820def25f87ec6ee1c7b072867351
Enzo Silveira <163486167+enzosas@users.noreply.github.com>
2025-11-05 10:37:37 -0300

    fix: Aprimorar scroll usando as setas

--- frontend/src/pages/orcamento/components/ModelSelector/ModelSelectorList.jsx
+++ frontend/src/pages/orcamento/components/ModelSelector/ModelSelectorList.jsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect, useRef } from "react";
 function ModelSelectorList({ modelos, onSelect }) {
     const [selecionado, setSelecionado] = useState(0);
     const containerRef = useRef(null);
+    const scrollContainerRef = useRef(null);
 
     useEffect(() => {
         if (modelos.length > 0) {
@@ -16,17 +17,42 @@ function ModelSelectorList({ modelos, onSelect }) {
         const handleKeyDown = (e) => {
             if (modelos.length === 0) return;
 
+            const scrollEl = scrollContainerRef.current;
+            if (!scrollEl) return;
+
             if (e.key === "ArrowDown") {
+                e.preventDefault();
+                
                 setSelecionado((prev) => {
                     const novo = (prev + 1) % modelos.length;
                     onSelect(modelos[novo]);
+
+                    const selectedItemEl = scrollEl.children[novo];
+                    if (selectedItemEl) {
+                        selectedItemEl.scrollIntoView({
+                            behavior: "smooth",
+                            block: "nearest",
+                        });
+                    }
                     return novo;
                 });
             }
+
             if (e.key === "ArrowUp") {
+                e.preventDefault();
+                
                 setSelecionado((prev) => {
                     const novo = (prev - 1 + modelos.length) % modelos.length;
                     onSelect(modelos[novo]);
+
+                    // 2. FAZ O SCROLL MANUAL
+                    const selectedItemEl = scrollEl.children[novo];
+                    if (selectedItemEl) {
+                        selectedItemEl.scrollIntoView({
+                            behavior: "smooth",
+                            block: "nearest",
+                        });
+                    }
                     return novo;
                 });
             }
@@ -50,7 +76,7 @@ function ModelSelectorList({ modelos, onSelect }) {
             tabIndex={0} // deixa o elemento focável
             ref={containerRef}
         >
-            <div className="selector">
+            <div className="selector" ref={scrollContainerRef}>
                 {modelos.map((modelo, index) => (
                     <div
                         key={index}
