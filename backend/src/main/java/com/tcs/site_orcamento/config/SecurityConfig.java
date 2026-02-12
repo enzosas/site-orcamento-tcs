ff03be776102450932b361662877c795db1c4f4e
Enzo Silveira <163486167+enzosas@users.noreply.github.com>
2026-02-12 08:25:05 -0300

    fix: Permitir acesso link deploy

--- backend/src/main/java/com/tcs/site_orcamento/config/SecurityConfig.java
+++ backend/src/main/java/com/tcs/site_orcamento/config/SecurityConfig.java
@@ -42,7 +42,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "https://enzosas.github.io"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
