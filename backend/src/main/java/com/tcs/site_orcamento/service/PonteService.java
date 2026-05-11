1b9f384025f556e44df5b31528ff3f0ac6ab7518
Enzo Silveira <163486167+enzosas@users.noreply.github.com>
2026-05-11 09:39:58 -0300

    feat: criar funcoes obter pesos

--- backend/src/main/java/com/tcs/site_orcamento/service/PonteService.java
+++ backend/src/main/java/com/tcs/site_orcamento/service/PonteService.java
@@ -12,6 +12,9 @@ public class PonteService {
     @Autowired
     MaxiprodService maxiprodService;
 
+    static Double precoTrilhoPorQuilo = 13.0;
+    static Double valorKgAco = 7.0;
+
     public static Double calculaCargaMaximaPorRoda(
             Double capacidadePonte, Double comprimentoPonte, Double pesoMetroLinear, Double pesoVigaPonte,
             Double pesoCabeceiraUtilizada) {
@@ -45,6 +48,66 @@ public class PonteService {
 
     public Double calculaValorParCabeceiras(Cabeceira cabeceira) {
         return maxiprodService.getPrecoDeVenda(cabeceira.getModelo());
+    }
+
+    public static Double getPesoTrilho(String tipoTrilho) {
+        if (tipoTrilho == null) {
+            return 0.0;
+        }
+        return switch (tipoTrilho) {
+            case "QD.32 mm" -> 8.04;
+            case "QD.38 mm" -> 11.51;
+            case "QD.44 mm" -> 15.2;
+            case "QD.51 mm" -> 20.42;
+            case "TR-25" -> 25.0;
+            case "TR-32" -> 32.0;
+            case "TR-37" -> 37.0;
+            case "ASCE 25" -> 12.0;
+            default -> 0.0;
+        };
+    }
+
+    public static Double getPesoColunaPorMetro(String tipoColuna) {
+        if (tipoColuna == null) {
+            return 0.0;
+        }
+
+        return switch (tipoColuna) {
+            case "100 x 100 x 4,75" -> 14.915;
+            case "100 x 100 x 6,35" -> 19.939;
+            case "127 x 127 x 4,75" -> 18.94205;
+            case "127 x 127 x 6,35" -> 25.32253;
+            case "150 x 150 x 4,75" -> 22.3725;
+            case "150 x 150 x 6,35" -> 29.9085;
+            case "175 x 175 x 6,35" -> 34.89325;
+            case "200 x 200 x 6,35" -> 39.878;
+            case "250 x 250 x 6,35" -> 48.58;
+            case "300 x 300 x 6,35" -> 58.55;
+            case "300 x 300 x 8,00" -> 73.35;
+            default -> 0.0;
+        };
+    }
+
+    public static Double getPesoFlangeColuna(String tipoColuna) {
+        if (tipoColuna == null) {
+            return 0.0;
+        }
+
+        return switch (tipoColuna) {
+            case "100 x 100 x 4,75", "100 x 100 x 6,35" -> 12.265625;
+            case "127 x 127 x 4,75", "127 x 127 x 6,35" -> 17.6625;
+            case "150 x 150 x 4,75", "150 x 150 x 6,35" -> 24.040625;
+            case "175 x 175 x 6,35" -> 34.221094;
+            case "200 x 200 x 6,35" -> 47.728;
+            case "250 x 250 x 6,35" -> 48.0;
+            case "300 x 300 x 6,35" -> 58.0;
+            case "300 x 300 x 8,00" -> 73.0;
+            default -> 0.0;
+        };
+    }
+
+    public static Double calculaPesoVigaPonte(Double comprimentoPonte, Double pesoMetroLinear) {
+        return comprimentoPonte * pesoMetroLinear / 1000;
     }
 
 }
