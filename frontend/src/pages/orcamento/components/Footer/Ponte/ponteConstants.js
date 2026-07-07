1f9c688baf4868fccb864f19b08de5b780c1524f
Enzo Silveira <163486167+enzosas@users.noreply.github.com>
2026-07-07 08:44:19 -0300

    feat: criar arquivo constantes ponte separado

---
+++ frontend/src/pages/orcamento/components/Footer/Ponte/ponteConstants.js
@@ -0,0 +1,124 @@
+export const opcoesFormaConstrutiva = [
+    "Univiga - Tipo Caixão",
+    "Univiga - Perfil W",
+    "Dupla Viga - Tipo Caixão"
+];
+
+export const opcoesCapacidade = [
+    "100",
+    "200",
+    "250",
+    "500",
+    "1000",
+    "1500",
+    "2000",
+    "3000",
+    "3200",
+    "5000",
+    "6000",
+    "6300",
+    "8000",
+];
+
+export const opcoesEletrificacaoTransversal = [
+    "Esteira porta cabo",
+    "Cabo Chato",
+    "Inexistente"
+];
+
+export const opcoesEletrificacaoLongitudinal = [
+    "Opcional",
+    "Barramento Blindado - 1 consumidor",
+    "Barramento Blindado - 2 consumidores",
+    "Barramento Blindado - 3 consumidores",
+    "Barramento Blindado - 4 consumidores",
+    "Somente o Carro coletor",
+    "Fornecido pelo Cliente",
+    "Inexistente"
+];
+
+export const opcoesTipoCaminhoRolamento = [
+    "Viga Metálica + Trilho",
+    "Somente Trilho",
+    "Trilho + Chumbador"
+];
+
+export const opcoesPerfilMetalico = [
+    "W150X13,0",
+    "W150X18,0",
+    "W200X15,0",
+    "W200X19,3",
+    "W200X22,5",
+    "W200X26,6",
+    "W200X31,3",
+    "W250X17,9",
+    "W250X22,3",
+    "W250X25,3",
+    "W250X28,4",
+    "W250X32,7",
+    "W250X38,5",
+    "W250X44,8",
+    "W310X21,0",
+    "W310X23,8",
+    "W310X28,3",
+    "W310X32,7",
+    "W310X38,7",
+    "W310X44,5",
+    "W310X52,0",
+    "W360X32,9",
+    "W360X39,0",
+    "W360X44,0",
+    "W360X51,0",
+    "W360X57,8",
+    "W360X64,0",
+    "W360X72,0",
+    "W360X79,0",
+    "W410X38,8",
+    "W410X46,1",
+    "W410X53,0",
+    "W410X60,0",
+    "W410X67,0",
+    "W410X75,0",
+    "W460X52,0",
+    "W460X60,0",
+    "W460X68,0",
+    "W460X74,0",
+    "W460X82,0",
+    "W460X89,0",
+    "W530X66,0",
+    "W530X72,0",
+    "W530X74,0",
+    "W530X82,0",
+    "W530X85,0",
+    "W530X92,0",
+    "W610X101,0",
+    "W610X113,0",
+    "W610X155,0",
+    "W610X174,0"
+];
+
+export const opcoesBitolaTrilho = [
+    "QD.32 mm",
+    "QD.38 mm",
+    "QD.44 mm",
+    "QD.51 mm",
+    "TR-25",
+    "TR-32",
+    "TR-37",
+    "ASCE 25"
+];
+
+export const opcoesDimensoes = [
+    "Sem coluna",
+    "100 x 100 x 4,75",
+    "100 x 100 x 6,35",
+    "127 x 127 x 4,75",
+    "127 x 127 x 6,35",
+    "150 x 150 x 4,75",
+    "150 x 150 x 6,35",
+    "175 x 175 x 6,35",
+    "200 x 200 x 6,35",
+    "250 x 250 x 6,35",
+    "300 x 300 x 6,35",
+    "300 x 300 x 8,00"
+];
