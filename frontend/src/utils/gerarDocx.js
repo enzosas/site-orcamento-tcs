bfde28f83d0200b6f99331b0f0c6b938dbdf7d56
Enzo Silveira <163486167+enzosas@users.noreply.github.com>
2026-07-08 10:19:54 -0300

    refactor: aprimorar legibilidade gerarDocx

--- frontend/src/utils/gerarDocx.js
+++ frontend/src/utils/gerarDocx.js
@@ -1,6 +1,6 @@
 import { TemplateHandler } from 'easy-template-x';
 import { saveAs } from 'file-saver';
-import templatePath from '../assets/templateOrcamento.docx?url';
+import templateSemPontePath from '../assets/templateOrcamento.docx?url';
 import templatePontePath from '../assets/templateOrcamentoPonte.docx?url';
 import { formatarTalhaExibicao, formatarConfigExibicao, getDadosExibicao, formatarConfigPonteExibicao, formatarPontePrecosPesos, gerarDescricaoCaminhoRolamento } from '../utils/dadosExibicao';
 
@@ -9,13 +9,14 @@ const formatadorPreco = new Intl.NumberFormat('pt-BR', { style: 'currency', curr
 export const gerarDocx = async (talha, config, cliente, precos, pagamento, arquivo = null, precosPesosPonte, ponteConfig) => {
     
     const incluiPonte = ponteConfig.incluir;
+    const templatePath = incluiPonte ? templatePontePath : templateSemPontePath;
 
     try {
         let templateBlob;
         if (arquivo) {
             templateBlob = await arquivo.arrayBuffer();
         } else {
-            const response = await fetch(incluiPonte ? templatePontePath : templatePath);
+            const response = await fetch(templatePath);
             if (!response.ok) throw new Error('Erro ao carregar o arquivo template.docx');
             templateBlob = await response.arrayBuffer();
         }
@@ -46,57 +47,44 @@ export const gerarDocx = async (talha, config, cliente, precos, pagamento, arqui
         const configPonteFormatada = formatarConfigPonteExibicao(ponteConfig)
         const precosPesosPonteFormatada = formatarPontePrecosPesos(precosPesosPonte)
 
-        const precoBaseTalha = pagamento.tipoPainel === "TCS" 
-            ? precos.totalTcs 
-            : precos.totalSch;
-
-        const precoTotalGeral = precoBaseTalha 
-            + Number(pagamento.ajusteTalha) 
-            + Number(pagamento.valorMontagem) 
-            + Number(pagamento.valorFrete);
-
-        const precoTotalGeralQuant = precoTotalGeral * Number(pagamento.quantidade);
-
-        const fraseCaminhoRolamento = gerarDescricaoCaminhoRolamento(ponteConfig);
-
+        const cepCidadeUF = `${cliente.cep || ''} - ${cliente.cidade || ''}/${cliente.estado || ''}`;
+        const correnteOuCabo = talha.correnteCabo === "Corrente" ? "Guia para Corrente" : "Guia para Cabo";
         const valorTotal = pagamento.orcamentoCalculado.precoTotal;
         const valorUnitario = pagamento.orcamentoCalculado.precoUnitario;
         const valorUnitarioTalhaFinal = pagamento.orcamentoCalculado.valorUnitarioTalhaFinal;
         const valorUnitarioPonteFinal = pagamento.orcamentoCalculado.valorUnitarioPonteFinal;
+        const data = new Date().toLocaleDateString('pt-BR', { day: 'numeric', month: 'long', year: 'numeric' })
+        const campoPonteEletrificacaoLongitudinal = configPonteFormatada.dadosBasicos_eletrificacaoLongitudinal + " - " + ponteConfig.dadosBasicos_comprimento + "m";
 
         const dados = {
-            cepCidadeUF: `${cliente.cep || ''} - ${cliente.cidade || ''}/${cliente.estado || ''}`,
-            correnteOuCabo: talha.correnteCabo === "Corrente" ? "Guia para Corrente" : "Guia para Cabo",
-            preco: formatadorPreco.format(precos.totalTcs || 0),
-            data: new Date().toLocaleDateString('pt-BR', { day: 'numeric', month: 'long', year: 'numeric' }),
-            imagemTalha: imagemObjeto || "",
             ...cliente,
             ...pagamento,
+            ...talhaFormatada,
+            ...configFormatada,
+            ...dadosTalha,
+            cepCidadeUF: cepCidadeUF,
+            correnteOuCabo: correnteOuCabo,
+            data: data,
+            imagemTalha: imagemObjeto || "",
             precoUnitarioF: formatadorPreco.format(valorUnitario),
             precoTotalF: formatadorPreco.format(valorTotal),
             precoUnitarioTalha: formatadorPreco.format(valorUnitarioTalhaFinal),
             precoUnitarioPonte: formatadorPreco.format(valorUnitarioPonteFinal),
-            ...talhaFormatada,
-            ...configFormatada,
-            ...dadosTalha,
             ponteOuPortico: configPonteFormatada.dadosBasicos_isPonte,
             ponteCapacidade: configPonteFormatada.dadosBasicos_capacidade,
             ponteVaoLivre: configPonteFormatada.dadosBasicos_vaoLivre,
             ponteTipo: configPonteFormatada.dadosBasicos_formaConstrutiva,
             ponteCaminhoRolamento: gerarDescricaoCaminhoRolamento(ponteConfig),
             ponteEletrificacaoTransversal: configPonteFormatada.dadosBasicos_eletrificacaoTransversal,
-            ponteEletrificacaoLongitudinal: configPonteFormatada.dadosBasicos_eletrificacaoLongitudinal + " - " + ponteConfig.dadosBasicos_comprimento + "m"
+            ponteEletrificacaoLongitudinal: campoPonteEletrificacaoLongitudinal,
         };
 
         const handler = new TemplateHandler();
         const doc = await handler.process(templateBlob, dados);
-
         const blobFinal = new Blob([doc], { 
             type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document" 
         });
-
         saveAs(blobFinal, `Orcamento_${cliente.razaoSocial || 'Cliente'}.docx`);
-
     } catch (error) {
         console.error(error);
         alert("Erro ao gerar documento: " + error.message);
